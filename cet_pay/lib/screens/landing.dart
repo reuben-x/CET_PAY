70a28fc7d010450d7b498f63c24140c15ecb1892
Kevinmt24 <kevinmt01@gmail.com>
2020-11-07 23:52:25 +0530

    Google Sign In Services

---
+++ cet_pay/lib/screens/landing.dart
@@ -0,0 +1,66 @@
+import 'package:cet_pay/services/auth.dart';
+import 'package:flutter/material.dart';
+class Landing extends StatefulWidget {
+  @override
+  _LandingState createState() => _LandingState();
+}
+
+class _LandingState extends State<Landing> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Overview'),
+        backgroundColor: Colors.grey,
+        elevation: 0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            onPressed: () async {
+              authService.signOut();
+            },
+            label: Text('Log Out'),
+          )
+        ],
+      ),
+      body: Column(
+          children: [
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 40, 30, 40),
+                  child: Text("Welcome Kevin",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),),
+                ),
+                SizedBox(width: 40,),
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: NetworkImage('https://picsum.photos/200'),
+                ),
+              ],
+            ),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const ListTile(
+                    title: Text("Payment Status",style: TextStyle(
+                      fontSize: 22,
+                    ),),
+                    subtitle: Text("Hostel Fees Paid"),
+                  ),
+                ],
+              ),
+            ),
+
+
+            Divider(),
+          ],
+      ),
+    );
+  }
+}
---
+++ cet_pay/lib/services/auth.dart
@@ -0,0 +1,65 @@
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
+
+class AuthService {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+ Observable<User> user;  //firebase user
+ Observable<Map<String, dynamic>> profile; //custom user data in firestore
+ PublishSubject loading = PublishSubject();
+
+ AuthService() {
+   print("Click");
+
+ }
+
+  Future<String> googleSignIn() async {
+   loading.add(true);
+   final GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+   final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+
+   final AuthCredential credential = GoogleAuthProvider.credential(
+     accessToken: googleSignInAuthentication.accessToken,
+     idToken: googleSignInAuthentication.idToken,
+   );
+
+   final UserCredential authResult = await _auth.signInWithCredential(credential);
+   final User user = authResult.user;
+
+   if (user != null) {
+     assert(!user.isAnonymous);
+     assert(await user.getIdToken() != null);
+
+     final User currentUser = _auth.currentUser;
+     assert(user.uid == currentUser.uid);
+
+     print('signInWithGoogle succeeded: $user');
+
+     return '$user';
+   }
+
+   return null;
+  }
+
+  void updateUserData (User user) async {
+   DocumentReference ref = _db.collection('users').doc(user.uid);
+
+   return ref.set({
+     'uid' : user.uid,
+     'email' : user.email,
+     'photoURL' : user.photoURL,
+     'displayName' : user.displayName,
+   },);
+  }
+
+  void signOut() {
+   _auth.signOut();
+  }
+
+
+}
+final AuthService authService = new AuthService();
