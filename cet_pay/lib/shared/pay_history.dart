194b906ceafa3b571da5f3cb5e737df7ded3cb83
reuben <reubenthomaspeter@gmail.com>
2020-12-17 00:07:06 +0530

    Expanding card implemented

--- cet_pay/lib/shared/pay_history.dart
+++ cet_pay/lib/shared/pay_history.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:expansion_card/expansion_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -5,69 +8,107 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class PaymentHistory extends StatefulWidget {
   @override
   _PaymentHistoryState createState() => _PaymentHistoryState();
-
 }
 
 class _PaymentHistoryState extends State<PaymentHistory> {
-  
-  Widget _buildListPanel(AsyncSnapshot<QuerySnapshot> snapshot) {
-
-    int listFreq = snapshot.data.docs.length;
-    return ExpansionPanelList(
-      expansionCallback: (int index,bool isExpanded) {
-        setState(() {
-
-        });
-      },
-      children: snapshot.data.docs.map((document) {
-        return ExpansionPanel(
-          headerBuilder: (BuildContext context, bool isExpanded) {
-            return ListTile(
-              title: Text('December 2020'),
-            );
-          },
-          body: ListTile(
-            title: Text(document['amount']),
-            subtitle: Text('This was Paid On'),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-            trailing: FlatButton(
-              onPressed: () {
-                //GO TO PAYMENTS FOLDER
-              },
-              child: Text('PAY NOW',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.green),),
-            ),
-          ),
-          isExpanded: false,
-        );
-      }).toList(),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        iconTheme: IconThemeData(color: Colors.black,),
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
         backgroundColor: Colors.transparent,
-        title: Text('Transaction History',style: TextStyle(color: Colors.black)),
+        title: Text(
+          'Transaction History',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('users').doc('mm6Z5bWY9RQlpgJSb0VPFcAmtal1').collection('fees').snapshots(),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .doc('mm6Z5bWY9RQlpgJSb0VPFcAmtal1')
+              .collection('fees')
+              .snapshots(),
           builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-           if (!snapshot.hasData) {
+            if (!snapshot.hasData) {
               return Center(
                 child: CircularProgressIndicator(
-                backgroundColor: Colors.green));
-              }
-          return _buildListPanel(snapshot);
-          }
-      ),
-     )
+                  backgroundColor: Theme.of(context).primaryColor,
+                ),
+              );
+            }
+            print(snapshot.data.docs);
+            return ListView(
+              children: snapshot.data.docs.map((document) {
+                return Center(
+                  child: ExpansionCard(
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          children: [
+                            Text("Nov 2020"),
+                            SizedBox(
+                              height: 7,
+                            ),
+                          ],
+                        ),
+                        document['paid_status'] == true
+                            ? Text(
+                                'Paid',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.green,
+                                    fontSize: 18),
+                              )
+                            : Text(
+                                'Not Paid',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red,
+                                    fontSize: 18),
+                              ),
+                      ],
+                    ),
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '\u{20B9} ${document['amount']}',
+                            style: TextStyle(fontSize: 40),
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                              horizontal: 7,
+                            ),
+                            child: OutlineButton(
+                              color: Theme.of(context).primaryColor,
+                              onPressed: () {
+                                //Initiate Payment
+                                print(document['date']);
+                              },
+                              child: Text(
+                                'PAY NOW',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            );
+          }),
     );
   }
 }
-
-
